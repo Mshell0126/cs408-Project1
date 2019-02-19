78da14fbfda917a695db9ce96b8163528f4abc6f
Marcus Shell <mshell2@stu.jsu.edu>
2019-02-19 14:23:39 -0600

    began layout for java implementation. added several methods and skeletons.

--- app/src/main/java/com/example/jsu/project1/Calculator.java
+++ app/src/main/java/com/example/jsu/project1/Calculator.java
@@ -8,8 +8,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.*;
 
 public class Calculator extends AppCompatActivity {
+
+    private float acc;
+    private float reg;
+    private String outputText;
+    private String lastOp;
+
+    boolean stateTerminal; // for determining if the text view should concatenate input or replace text with input.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,64 @@ public class Calculator extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        lastOp = "";
+        clear();
+    }
+
+    public void onClick(Button b){
+        String id = (b.getResources().getResourceName(b.getId())).split("/")[1];
+        //insert code
+
+        updateView();
+
+    }
+
+    private void concatenate(String input){
+        //
+    }
+
+    private void add(){
+        acc = acc + reg;
+    }
+
+    private void divide(){
+        try {
+            acc = reg / acc;
+        }
+        catch(Exception e) {
+            if (e.getCause().equals("division by zero")) {
+                outputText = "undefined";
+                //update screen with error. CHECK HOW TO EXAMINE ERROR TYPES!
+            } else {
+                outputText = "err";
+            }
+        }
+    }
+
+    private void sqrt(){
+
+    }
+
+    private void percentage(){
+
+    }
+
+    private void equals(){
+        //perform last operation with acc and reg (call method)
+        updateView();
+    }
+
+    private void clear(){
+        acc = 0;
+        reg = 0;
+        outputText = "0";
+
+
+    }
+
+    private void updateView(){
+        TextView outputWindow = (TextView) findViewById(R.id.output);
+        outputWindow.setText(outputText);
     }
 
     @Override
